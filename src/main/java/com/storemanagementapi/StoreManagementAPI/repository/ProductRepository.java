78c13f088b308dccad1d86507c38b9d1695fe4bb
kronos2713 <60075225+kronos2713@users.noreply.github.com>
2022-04-20 15:48:35 +0300

    updated ProductRepository interface

--- src/main/java/com/storemanagementapi/StoreManagementAPI/repository/ProductRepository.java
+++ src/main/java/com/storemanagementapi/StoreManagementAPI/repository/ProductRepository.java
@@ -1,9 +1,10 @@
 package com.storemanagementapi.StoreManagementAPI.repository;
 
 
+import com.storemanagementapi.StoreManagementAPI.datamodel.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface ProductRepository extends JpaRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
