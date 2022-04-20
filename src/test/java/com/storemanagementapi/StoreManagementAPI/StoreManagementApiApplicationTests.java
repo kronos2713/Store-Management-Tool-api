923115d173316f381abab7a0ad72be20d67f5607
kronos2713 <60075225+kronos2713@users.noreply.github.com>
2022-04-20 14:18:31 +0300

    initial commit

---
+++ src/test/java/com/storemanagementapi/StoreManagementAPI/StoreManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storemanagementapi.StoreManagementAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
