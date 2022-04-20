334e9fa57061a6905e1bec6115a0cb5bad016fa4
kronos2713 <60075225+kronos2713@users.noreply.github.com>
2022-04-20 15:13:27 +0300

    defined  data model for Product class

--- src/main/java/com/storemanagementapi/StoreManagementAPI/datamodel/Product.java
+++ src/main/java/com/storemanagementapi/StoreManagementAPI/datamodel/Product.java
@@ -1,10 +1,87 @@
 package com.storemanagementapi.StoreManagementAPI.datamodel;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private double price;
+
+    @Column(name = "stock")
+    private boolean stock;
+
+    @Column(name = "quantity")
+    private int count;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, double price, boolean stock, int count) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.stock = stock;
+        this.count = count;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isInStock() {
+        return stock;
+    }
+
+    public void setInStock(boolean inStock) {
+        this.stock = inStock;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 
 }
